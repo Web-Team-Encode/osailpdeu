7af9c234fc19c430ca39a92f47474e4e47bc6d2e
Preet Sojitra <preet.dev373@gmail.com>
2023-02-13 12:46:38 +0530

    chore: add routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 
 function App() {
   return (
-    <div className="text-4xl ">
-      <h1 className="text-center">OSAIL</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/clubs" element={<h1>Clubs</h1>} />
+          <Route path="/committees" element={<h1>Committees</h1>} />
+          <Route path="/epf" element={<h1>EPF</h1>} />
+          <Route path="/calendar" element={<h1>Calendar</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
