30376fd410f9e64578b24966718dba999c04fa72
Preet Sojitra <preet.dev373@gmail.com>
2023-02-13 12:33:47 +0530

    chore: defined color scheme in tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        whitePrimary: "#FDF7E7",
+        blueSecondary: {
+          100: "#81A6B8",
+          800: "#1F3D53",
+        },
+        redTertiary: "#F76B52",
+      },
+    },
   },
   plugins: [],
 }
